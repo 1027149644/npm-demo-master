ecc56237a7a4c46262fd7b42d8c7005d126617cc
1027149644@qq.com <1027149644@qq.com>
2021-03-15 16:15:28 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <Component1></Component1>
+    </div>
+</template>
+
+<script>
+import Component1 from './components/Component1.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Component1,
+  },
+};
+</script>
+
+<style>
+    #app {
+        margin: 0;
+        padding: 0;
+    }
+</style>
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Component1 from './Component1.vue';
+
+Component1.install = Vue => Vue.component(Component1.name, Component1);
+
+export default Component1;
